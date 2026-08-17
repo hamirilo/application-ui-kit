d581379470024a1353c612cdf4d0bd8d921a01ee
hamirilo <t.quartet@gmail.com>
2026-08-17 19:10:11 +0900

    chore: simplify public Storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,41 +1,26 @@
-import type { StorybookConfig } from '@storybook/react-vite'
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import tailwindcss from '@tailwindcss/vite'
-
-const here = dirname(fileURLToPath(import.meta.url))
-/** Application UI Kit root */
-const pkgRoot = resolve(here, '..')
+import type { StorybookConfig } from "@storybook/react-vite";
+import tailwindcss from "@tailwindcss/vite";
 
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../stories/**/*.stories.@(ts|tsx)',
-  ],
-
-  addons: ['@storybook/addon-docs', '@storybook/addon-a11y'],
-
+  stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(ts|tsx)"],
+  addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
   framework: {
-    name: '@storybook/react-vite',
+    name: "@storybook/react-vite",
     options: {},
   },
-
   typescript: {
-                reactDocgen: 'react-docgen-typescript',
+    reactDocgen: "react-docgen-typescript",
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
       shouldRemoveUndefinedFromOptional: true,
-                  propFilter: (prop) =>
+      propFilter: (prop) =>
         prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
     },
   },
+  viteFinal: async (viteConfig) => ({
+    ...viteConfig,
+    plugins: [...(viteConfig.plugins ?? []), tailwindcss()],
+  }),
+};
 
-  viteFinal: async (viteConfig) => {
-    return {
-      ...viteConfig,
-      plugins: [...(viteConfig.plugins ?? []), tailwindcss()],
-    }
-  },
-}
-
-export default config
+export default config;
